b40c021f8f8bc6d80de34c4859d3f63abd87f69e
Your Name <fantastizeey@gmail.com>
2024-07-08 14:02:35 +0100

    well i hope i broke nothing

--- src/components/Landingpage/Slider.jsx
+++ src/components/Landingpage/Slider.jsx
@@ -1,5 +1,5 @@
 // Slider.js
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Btn from "./Btn";
 import rightarrow from "/Arrowright.svg";
 import PropTypes from "prop-types";
@@ -8,7 +8,7 @@ export const slidesData = [
   {
     title: "Prescription Drugs",
     description:
-      "Pharmasynthesis have a wide variety of prescription drugs which can only be obtained with a valid prescription from a licensed healthcare provider",
+      "Pharmasynthesis has a wide variety of prescription drugs which can only be obtained with a valid prescription from a licensed healthcare provider",
     buttonText: "Shop Now",
   },
   {
@@ -31,45 +31,17 @@ export const slidesData = [
   },
 ];
 
-const Slider = ({ slides = [] }) => {
-  // const [currentSlide, setCurrentSlide] = useState(0);
-  // const [direction, setDirection] = useState(1); // 1 for forward, -1 for backward
-
-  // useEffect(() => {
-  //   if (slides.length === 0) return;
-
-  //   const slideInterval = setInterval(() => {
-  //     setCurrentSlide((prevSlide) => {
-  //       const nextSlide = prevSlide + direction;
-  //       if (nextSlide >= slides.length || nextSlide < 0) {
-  //         setDirection(-direction); // Reverse direction
-  //         return prevSlide; // Stay on the current slide
-  //       }
-  //       return nextSlide;
-  //     });
-  //   }, 3000);
-
-  //   return () => clearInterval(slideInterval);
-  // }, [direction, slides.length]);
-
-  // if (slides.length === 0) {
-  //   return <div>No slides available</div>;
-  // }
-
+const Slider = ({ slides = slidesData }) => {
   return (
-    <section className="relative  md:ml-[150px] overflow-hidden h-[350px] mb-5 md:mb-[100px]">
-      <div
-        className="flex  transition-transform duration-500"
-        style={{ transform: `translateX(-${currentSlide * 350}px)` }}
-      >
+    <section className="relative md:ml-[120px] mt-9 md:overflow-hidden md:h-[350px] mb-5 md:mb-[100px]">
+      <div className="flex  justify-center items-center md:flex-row flex-col gap-5 ">
         {slides.map((slide, index) => (
-          <div
-            key={index}
-            className="w-[350px] h-[280px] ml-[30px] flex-shrink-0"
-          >
+          <div key={index} className="w-[280px] h-[280px]  flex-shrink-0">
             <div className="px-[36px] pb-[18px] pt-[50px] bg-white shadow-xl rounded-lg relative h-full">
-              <h3 className="text-[20px] font-bold mb-[13px]">{slide.title}</h3>
-              <p className="text-[#0c0c0c] mb-[37px]">{slide.description}</p>
+              <h3 className="text-[18px] font-bold mb-[13px]">{slide.title}</h3>
+              <p className="text-[#0c0c0c] text-[14px] mb-[37px]">
+                {slide.description}
+              </p>
               <Btn
                 title="Shop Now"
                 linkpath="/shop"
