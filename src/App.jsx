e1c3403398eafc6e98d35c4429c654231f6071c2
Your Name <fantastizeey@gmail.com>
2024-07-22 18:46:45 +0100

    we move lol fixing build error

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,7 @@ import Success from "./components/register/succes";
 import SignIn from "./components/signIn/signIn";
 import SignUp from "./components/register/SignUp";
 import Success2 from "./components/signIn/success2";
-import RegisterPhamComp from "./components/register/Register2PhamComp";
-import Register2phamarcy from "./components/register/Register2phamarcy";
-import RegisterPersonel from "./components/register/RegisterPersonel";
+
 import SignIn2 from "./components/signIn/signin2";
 import Dashboard from "./components/Dash/Dashboard";
 import Home from "./components/Landingpage/Home";
@@ -31,9 +29,6 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/Register" element={<Register />} />
         <Route path="/Register2" element={<Register2 />} />
-        {/* <Route path="/RegisterPhamComp" element={<RegisterPhamComp />} />
-        <Route path="/Register/Pharmacy" element={<Register2phamarcy />} />
-        <Route path="/Register/personel" element={<RegisterPersonel />} /> */}
         <Route path="/Success" element={<Success />} />
         <Route path="/PasswordSuccess" element={<Success3 />} />
         <Route path="/SignUp" element={<SignUp />} />
@@ -41,7 +36,6 @@ function App() {
         <Route path="/SignIn2" element={<SignIn2 />} />
         <Route path="/Signin-Success" element={<Success2 />} />
         <Route path="/Dashboard/*" element={<Dashboard />} />
-        {/* <Route path="/Dashboard/users" element={<Users />} /> */}
         <Route path="/Forgotpassword" element={<Forgotpassword />} />
         <Route path="/Forgotpassword2" element={<Forgotpassword2 />} />
         <Route path="/ChangePassword" element={<ChangePassword />} />
